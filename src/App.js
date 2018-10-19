f7246dfcedbbf77e3b639066afc15bb919d0112a
Simon Halimonov <hello@simonhalimonov.de>
2018-10-19 11:24:52 +0200

    QR Reader

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import QrReader from 'react-qr-reader'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Change the text
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
+          <QRCamera />
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+class QRCamera extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      delay: 300,
+      result: 'No result'
+    }
+    this.handleScan = this.handleScan.bind(this)
+  }
+  handleScan(data) {
+    if (data) {
+      this.setState({
+        result: data
+      })
+    }
+  }
+  handleError(err) {
+    console.error(err)
+  }
+  render() {
+    return (
+      <div>
+        <QrReader
+          delay={this.state.delay}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          style={{ width: 500 }}
+        />
+        <p>{this.state.result}</p>
+      </div>
+    )
+  }
+}
+
+export default App
