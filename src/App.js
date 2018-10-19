36c161530fbede7c8b429fb1bd17b0e5888d2114
Simon Halimonov <hello@simonhalimonov.de>
2018-10-19 11:39:29 +0200

    QRCode Generator

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react'
 import './App.css'
 
 import QrReader from 'react-qr-reader'
+import QRCode from 'qrcode.react'
 
 class App extends Component {
+  state = {qrcode: ""}
+
+  handleChange = () => {
+    this.setState({qrcode: this.refs.qrcode.value})
+    console.log(this.state);
+    
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          
+          <input onChange={this.handleChange} type="text" ref="qrcode"/>
+          <QRCode
+          value={this.state.qrcode}
+            size={256}
+            bgColor={"#ffffff"}
+            fgColor={"#000000"}
+            level={"L"}
+            style={{padding: 50}}
+          />
           <QRCamera />
         </header>
       </div>
